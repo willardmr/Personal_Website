89f0eb43015fb4234b042474cfdeca4c3eddb294
Max <maxwell.rubin.willard@gmail.com>
2016-02-02 19:38:42 -0600

    Trying out different url and cdn calls

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,12 +3,9 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from django.views.generic import RedirectView
-
 from hello.views import AboutView, create_model
 
 
 urlpatterns = patterns('',
                        url(r'^$', AboutView.as_view(), name='index'),
-						url(r'^create_post/$', create_model),
-						(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico/')),)
+						url(r'^create_post/$', create_model))
