c4b16c63e88ec6673d38334d90e2299102170009
Max <maxwell.rubin.willard@gmail.com>
2016-09-29 19:40:18 -0500

    Flake8 and docstrings added.

--- personal_app/apps.py
+++ personal_app/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
 
+
 class MyAppConfig(AppConfig):
     name = 'personal_app'
 
     def ready(self):
-    	import personal_app.signals
+        import personal_app.signals # NoQA
--- personal_app/models.py
+++ personal_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
+
 class SimpleModel(models.Model):
+    """
+    Basic model for example purposes
+    """
     value = models.IntegerField()
